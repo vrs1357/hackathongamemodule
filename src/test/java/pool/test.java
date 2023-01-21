56e46d830e3ef0605792da5564f1f1329e1def94
vrs1357 <varunseshadri13@gmail.com>
2023-01-21 13:56:07 -0800

    im stupid

--- src/test/java/pool/test.java
+++ src/test/java/pool/test.java
@@ -1,5 +1,12 @@
 package test.java.pool;
 
+import main.java.pool.*;
+
+import org.junit.Test;
+
+
 public class test {
-    
+    public void test1(){
+        assertEquals("Hello World", testCode());
+    }
 }
