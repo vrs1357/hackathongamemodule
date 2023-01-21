603c06f3c18e7afbd03e564ca601375af4718b2d
vrs1357 <varunseshadri13@gmail.com>
2023-01-21 14:15:45 -0800

    where did the main go

--- src/main/java/pool/main.java
+++ src/main/java/pool/main.java
@@ -5,9 +5,5 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
-    public String testCode(){
-        return "Hello World";
-    }
 }
 
