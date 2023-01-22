80aefb98b800cc50a6a72de6691bb18596b95d21
Chirag Deepak <chirag07d@gmail.com>
2023-01-21 17:46:56 -0800

    last edit for now

--- pool.js
+++ pool.js
@@ -1,14 +1,6 @@
-const soundIcon = document.getElementById("sound-icon");
-let isSoundMuted = false;
-
-soundIcon.addEventListener("click", function() {
-  if (isSoundMuted) {
-    // unmute the sound
-    isSoundMuted = false;
-    soundIcon.src = "https://icon-library.com/images/sound-icon-vector/sound-icon-vector-3.jpg";
-  } else {
-    // mute the sound
-    isSoundMuted = true;
-    soundIcon.src = "https://icon-library.com/images/sound-icon-vector/sound-icon-vector-3.jpg";
-  }
-});
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", runJarFile);
+function runJarFile() {
+    java -jar Billiard.jar
+ }
+ 
